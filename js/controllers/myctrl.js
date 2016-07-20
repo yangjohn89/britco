dc7a71b3c59411a9573134de7c22b0051b12868c
Yang Yu <Yang Yu>
2016-07-19 22:40:10 -0700

    This is Britco project

---
+++ js/controllers/myctrl.js
@@ -0,0 +1,47 @@
+app.controller('myCtrl', function($scope) {
+	$scope.product1 = {
+		img: "img/product1.jpg",
+		description: "Knotted Cord Necklace Kit",
+		price: 29.00
+	}
+	$scope.product2 = {
+		img: "img/product2.jpg",
+		description: "Wood Burning 101: Custom Pineapple Cheese Board",
+		price: 59.00
+	}
+	$scope.product3 = {
+		img: "img/product3.jpg",
+		description: "Embrodered Typography Kit",
+		price: 29.00
+	}
+	$scope.product4 = {
+		img: "img/product4.jpg",
+		description: "Engraving 101: Custom Copper Mugs",
+		price: 39.00
+	}
+	$scope.product5 = {
+		img: "img/product5.jpg",
+		description: "Wood Burning 101: Custom Kitchen Utensils",
+		price: 59.00
+	}
+	$scope.product6 = {
+		img: "img/product6.jpg",
+		description: "Color Wrapped Bracelets Kit",
+		price: 19.99
+	}
+	$scope.product7 = {
+		img: "img/product7.jpg",
+		description: "Gold Foil Tote Kit by SUGAR & CLOTH",
+		price: 22.00
+	}
+	$scope.product8 = {
+		img: "img/product8.jpg",
+		description: "Watercolor Gold Leaf Cake Kit by SPRINKLE BAKES",
+		price: 34.00
+	}
+	$scope.product9 = {
+		img: "img/product9.jpg",
+		description: "Leather Jewelry Roll Kit by CHLOE & ISABEL",
+		price: 39.00
+	}
+})
